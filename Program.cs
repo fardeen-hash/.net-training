a733def2f6a04c4ef1087742da8b83cecfe8d8f3
fardeen-hash <needraf007@gmail.com>
2024-07-04 15:02:06 +0530

    DONE

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        var calculator = new Calculator();
+        Console.WriteLine(calculator.Add(1,2));
+        Console.WriteLine(calculator.Add(1,2,3));
+        Console.WriteLine(calculator.Add(new int[]{1,2,3,4,69}));
+
+
+
+    }
+}
